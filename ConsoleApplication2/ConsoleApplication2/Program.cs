9385ee28b50ac232c66e52fdf7a9facf0578725d
PandaLee <phanhuy0108@gmail.com>
2019-11-14 17:29:33 +0700

    chinh sua, remove, add :done

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OOPCSEntities db = new OOPCSEntities();
+            var sum = 0;
+            var expentitures = db.Expenditures.ToArray();
+            for (int i = 0; i < expentitures.Length; i++)
+            {
+                sum += expentitures[i].Price;
+            }
+            Console.WriteLine("Tong Tien La : {0}", sum);
+        }
+    }
+}
